5cd59167ac92ef42ec027d461a2b5784f468cf68
RaymondOchoa <ochoaraymondb@yahoo.com>
2023-01-08 23:57:49 -0800

    week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+const { objectExpression } = require("@babel/types")
+const { number } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,6 +18,17 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+//Psuedocode: 
+// Input: ["purple", "blue", "green", "yellow", "pink"]
+//        ["chartreuse","indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// Output: ["yellow", "blue", "pink", "green"]
+//         ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+// write a test that will include expect and to euqal statements for the variables provided
+// run the test and see it fail it should be a not defined error
+// create a function 
+// console log the test and variables
+// test should pass
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = [
@@ -27,9 +41,47 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-// b) Create the function that makes the test pass.
+describe("colorShuffle", () => {
+  it ("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+    expect(colorShuffle(colors1)).toEqual(["yellow", "blue", "pink", "green"]) 
+    expect(colorShuffle(colors2)).toEqual(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"])
+  })
+})
 
+// FAIL  ./code-challenges.test.js
+//   colorShuffle
+//     ✕ takes in an array, removes the first item from the array and shuffles the remaining content (1 ms)
+
+//   ● colorShuffle › takes in an array, removes the first item from the array and shuffles the remaining content
+
+//     ReferenceError: colorShuffle is not defined
+
+// b) Create the function that makes the test pass.  
+const colorShuffle = (colors) => {
+  let shuffledColors = [...colors]
+  for(let i=0; i<colors.length; i++){
+    colors.shift(i)
+    const shuffle = Math.floor(Math.random () * (i+1))
+    var temp = colors[i]
+    colors[i] = shuffledColors
+    shuffledColors = temp
+    shuffledColors(shuffle)
+  }
+} 
+
+console.log(colorShuffle(colors1))
+console.log(colorShuffle(colors2))
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
+
+//Psuedocode: 
+// Input: { upVotes: 13, downVotes: 2 }
+//        { upVotes: 2, downVotes: 33 }
+// Output: 11 & -31
+// write a test that will include expect and to euqal statements for the variables provided
+// run the test and see it fail it should be a not defined error
+// create a function 
+// console log the test and variables
+// test should pass
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -38,9 +90,40 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe("totalVotes", () => {
+  it ("takes in an object that contains up votes and down votes and returns the end tally", () => {
+    expect(totalVotes(votes1)).toEqual([11]) 
+    expect(totalVotes(votes2)).toEqual([-31])
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+//   totalVotes
+//   ✕ takes in an object that contains up votes and down votes and returns the end tally
+
+// ● totalVotes › takes in an object that contains up votes and down votes and returns the end tally
+
+//   ReferenceError: totalVotes is not defined
+
 // b) Create the function that makes the test pass.
 
+const totalVotes = (upVotes, downVotes) => {
+  const combinedVotes = this.upVotes + this.downVotes
+}
+console.log(totalVotes(votes1))
+console.log(totalVotes(votes2))
+
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+
+//Psuedocode: 
+// Input: ["array", "object", "number", "string", "Boolean"]
+//        ["string", "null", "Boolean", "string", "undefined"]
+// Output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+// write a test that will include expect and to euqal statements for the variables provided
+// run the test and see it fail it should be a not defined error
+// create a function 
+// console log the test and variables
+// test should pass
 
 // a) Create a test with an expect statement using the variables provided.
 
@@ -48,4 +131,29 @@ const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+describe("noRepeat", () => {
+  it ("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+    expect(noRepeat(dataArray1, dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"]) 
+  })
+})
+// FAIL  ./code-challenges.test.js
+//   noRepeat
+//     ✕ takes in two arrays as arguments and returns one array with no duplicate values (1 ms)
+
+//   ● noRepeat › takes in two arrays as arguments and returns one array with no duplicate values
+
+//     ReferenceError: noRepeat is not defined
+
 // b) Create the function that makes the test pass.
+
+const noRepeat = (dataArray1, dataArray2) => {
+ const mixedDataArray = dataArray1.concat(dataArray2)
+ mixedDataArray.forEach(Element => {
+  if (!mixedDataArray.includes(Element)) {
+    mixedDataArray.push(Element)
+  } 
+ });
+
+
+} 
+console.log(noRepeat(dataArray1, dataArray2))
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,42 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object oriented programming is a style of programming that focuses on data and object rather than logic or functions.
 
-Researched answer:
+Researched answer: Object oriented programming is a programming style that focuses on data and objects. This method comes with plenty of reusable code that can help streamline the process making it a scalable and collaborative-friendly approach to programming. OOP utilizes classes and objects to handle instances of logging multiple things and having them perform the same actions, these classes act as blueprints for the structure of the methods and attributes passed. 
 
 1. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: An integer is a a regular number whether negative or positive but a Float is a a negative or positive number with a decimal.
 
-Researched answer:
+Researched answer: Integers and floats are both types of numerical data, the main difference between the two is that inters are whole numbers wheras floats are integers. Floats are used to get a more accurate and precise numerical value because floats have a higher maximum value due to the fact they allow for larger exponents/values. 
 
 1. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: An implicit return is a method of returning values from a function without specifically writing return, it has pre built in code to recognize what you are trying to do with less input needed when compared to javascript/react.
 
-Researched answer:
+Researched answer: An implicit return is a return that implies the concept of return without explicitly writing out it out. This can be achieved through the use of arrow functions and occasionally parenthesis although because it is an implicit return there are cases where the parenthesiss are not required. An example of this would be if you only had one parameter in the arrow function. In any other case, multiple parameters would need to be wrapped in parenthesis.
 
 1. What is a block in Ruby?
 
-Your answer:
+Your answer: Ruby blocks are anonymous functions that can be passed into a method. Occasionally they will contain arguments that can be defined by two pipe characters (Example: |x| )
 
-Researched answer:
+Researched answer: Blocks are similar to methods but they do not belong to an object. Blocks are set in a do/end statement or between brackets, they can also hold multiple arguments separated by tow pipe | characters. These are useful because they allow you to save bits of code and reuse them later expediting the process when comparing two elements for example. 
 
 1. How do you extract a value in a Ruby hash?
 
-Your answer:
+Your answer: To extract a value in a Ruby hash you have to log the name of the hash with the key name set within brackets and following a colon (example- p palindrome[:racecar])
 
-Researched answer:
+Researched answer: In Ruby the values in hashes can be accessed using bracket notation after the hash name you can type the name of the key. Extracting the value would look like this "p recipe[:sugar]". This is useful when you only need to display bits and pieces of the hash. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Class Inheritance:
+1. Class Inheritance: Class inheritance is when a class derives from another class, the child case is essentially gathering its properties and methods from the parent class.
 
-2. RSpec:
+2. RSpec: RSpec is a testing tool written into Ruby to test the code. 
 
-3. CRUD:
+3. CRUD: Acronym for persistent storage that stands for Create, Read, Update, Delete.
 
-4. Test-driven development:
+4. Test-driven development: Method of software developement that focuses on turning software requirements into tests and tracking all the software developement by testing the software against all cases.
 
-5. HTTP:
+5. HTTP: HTTP is a protocol for fetching resources such as HTML documents, the acronym stands for Hypertext Transfer Protocol.
